40996243d254d4113038d5bf33f3627a943c28f8
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-07-27 22:42:51 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = [n];
+    while(n != 1){
+        if(n%2 == 1){
+            n = 3*n + 1
+        }else{
+            n = n/2
+        }
+        answer.push(n);
+    }
+    return answer;
+}
