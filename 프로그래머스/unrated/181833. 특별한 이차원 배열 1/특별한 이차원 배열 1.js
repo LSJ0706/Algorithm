afbabe14cc4d03e941f4ae9cd23d5ae25723a420
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-09-04 22:43:43 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.10 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = [];
+    
+    for(let i=0; i<n; i++) {
+        var arr = new Array(n).fill(0)
+        arr[i] = 1
+        answer.push(arr)
+        }
+    return answer;
+}
