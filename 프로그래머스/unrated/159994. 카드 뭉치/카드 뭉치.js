fd0853e818b6f89d87477afce8050d8f0e517555
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-09-21 15:46:38 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    var idx = 0;
+    
+    while(idx < goal.length) {
+        if(cards1[0] === goal[idx]) {
+            cards1.shift()
+            idx++
+        }else if(cards2[0] === goal[idx]) {
+            cards2.shift()
+            idx++
+        }else {
+            break;
+        }
+    }
+
+    return idx === goal.length ? 'Yes' : 'No';
+}
