f2c927b178f496eb3d1484b5a88599446b6aab10
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-11-02 16:50:08 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.49 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,9 @@
+function solution(n, left, right) {
+    var answer = [];
+    for(let i=left; i<=right; i++) {
+        const x = Math.floor(i/n)
+        const y = i%n
+        answer.push(Math.max(x, y)+1)
+    }
+    return answer;
+}
