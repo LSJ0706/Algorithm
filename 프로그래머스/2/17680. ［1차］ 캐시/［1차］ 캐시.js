e1528729f26ed7040e4d29ecd6f13243e4d6df35
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-11-06 23:18:48 +0900

    [level 2] Title: [1차] 캐시, Time: 5.30 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,21 @@
+function solution(cacheSize, cities) {
+    cities = cities.map((x) => x.toLowerCase())
+    var answer = 0;
+    var stack = [];
+    cities.forEach((e) => {
+        if(!stack.includes(e)) {
+            answer += 5
+            stack.push(e)
+            if(stack.length > cacheSize) {
+                stack.shift();
+            }
+        }else {
+            answer++;
+            let idx = stack.indexOf(e)
+            stack.splice(idx,1)
+            stack.push(e)
+        }
+    })
+    console.log(stack)
+    return answer;
+}
