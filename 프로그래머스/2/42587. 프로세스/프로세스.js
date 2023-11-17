e327b5b9784d8e812b829e440871f3169b275d0d
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-11-17 17:07:08 +0900

    [level 2] Title: 프로세스, Time: 0.16 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,18 @@
+function solution(priorities, location) {
+    let queue = Array.from(new Array(priorities.length).keys());
+    let maxValue
+    let answer = 0;
+    
+    while(true) {
+        maxValue = Math.max(...priorities);
+        
+        if(priorities[0] < maxValue) {
+            priorities.push(priorities.shift());
+            queue.push(queue.shift());
+        }else {
+            answer++
+            priorities.shift();
+            if(location == queue.shift()) return answer;
+        }
+    }
+}
