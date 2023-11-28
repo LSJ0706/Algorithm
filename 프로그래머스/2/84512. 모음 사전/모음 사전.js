a1bea049d593fff7600866a896fa21b4cf1ee82b
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2023-11-28 18:25:12 +0900

    [level 2] Title: 모음 사전, Time: 3.29 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,20 @@
+function solution(word) {
+    var answer = 0;
+    const result = [];
+    
+    const DFS = (word, length, result) => {
+        const gather = [..."AEIOU"];
+        
+        if(length === word.length) {
+            result.push(word)
+            return;
+        }
+        
+        gather.forEach((e) => {
+            DFS(word+e, length, result);
+            });
+        };
+    
+    for(let i=1; i<=5; i++) DFS("", i, result)
+    return result.sort().indexOf(word) + 1;
+}
