0b3ff0412dfd50f92d447ba1fa600c11a6f030f9
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-01-30 18:32:45 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 2.67 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -0,0 +1,42 @@
+function solution(m, n, board) {
+    let answer = 0;
+    board = board.map((v) => v.split(""));
+    while(true) {
+        const clear = [];
+        for(let i=0; i<m-1; i++) {
+            for(let j=0; j<n-1; j++) {
+                if(board[i][j] == board[i][j+1] &&
+                   board[i][j] == board[i+1][j] &&
+                   board[i][j] == board[i+1][j+1] &&
+                   board[i][j] != 0) {
+                    clear.push([i,j]);
+                }
+            }
+        }
+        if(clear.length == 0) {
+            return [].concat(...board).filter((v) => v == 0).length;
+        }
+        for(let i=0; i<clear.length; i++) {
+            const [x,y] = clear[i]
+            board[x][y] = 0;
+            board[x+1][y] = 0;
+            board[x][y+1] = 0;
+            board[x+1][y+1] = 0;
+        }
+        for(let i=m-1; i>=0; i--) {
+            for(let j=0; j<n; j++) {
+                if(board[i][j] == 0) {
+                    for(let k=i-1; k>=0; k--) {
+                        if(board[k][j] != 0) {
+                            board[i][j] = board[k][j];
+                            board[k][j] = 0;
+                            break;
+                    }
+                }
+            }
+            }
+        }
+        
+    }
+    return answer;
+}
