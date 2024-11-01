456752df87053d7539c815bcd9e9980211d70476
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-01 19:47:57 +0900

    [level 1] Title: 둘만의 암호, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,13 +1,16 @@
 function solution(s, skip, index) {
-    var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    alpha = alpha.filter((e) => !skip.includes(e))
-    var answer = [...s].map((v) => {
-        if(alpha.length <= alpha.indexOf(v)+index) {
-            v = alpha[(alpha.indexOf(v)+index) % alpha.length]
-        }else {
-            v = alpha[alpha.indexOf(v)+index]
-        }
-        return v
-    })
+    const answer = [];
+    const alpha =  ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                    'n','o','p','q','r','s','t','u','v','w','x','y','z',];
+    const filter = alpha.filter((v) => !skip.includes(v));
+    for(let i=0; i<s.length; i++) {
+        let temp = filter.indexOf(s[i]);
+        
+        if(temp + index >= filter.length) temp = (temp+index)%filter.length;
+        else temp = temp + index;
+        
+        answer.push(filter[temp]);
+    }
     return answer.join("");
 }
+//alpha 26
