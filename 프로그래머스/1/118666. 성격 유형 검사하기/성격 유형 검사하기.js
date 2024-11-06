f18f961636e38dd63e2ae3b59c9f8549f05632fe
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-06 16:01:07 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.56 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,31 +1,27 @@
 function solution(survey, choices) {
     var answer = '';
-    var person = {
-        'R' : 0,
-        'T' : 0,
-        'C' : 0,
-        'F' : 0,
-        'J' : 0,
-        'M' : 0,
-        'A' : 0,
-        'N' : 0,
-    }
-    var category = ['RT', 'CF', 'JM', 'AN']
+    const person = {
+        'R' : 0, 'T' : 0,
+        'C' : 0, 'F' : 0,
+        'J' : 0, 'M' : 0,
+        'A' : 0, 'N' : 0
+    };
+    const personality = ['RT', 'CF', 'JM', 'AN'];
+    
     for(let i=0; i<survey.length; i++) {
-        if(choices[i] > 4) {
-            person[survey[i].split("")[1]] += (choices[i] - 4)
-        } else if(choices[i] < 4){
-            person[survey[i].split("")[0]] += (4 - choices[i])
+        const choice = choices[i] - 4
+        if(choice < 0) {
+            person[survey[i][0]] += Math.abs(choice);
+        }
+        else if(choice >0) {
+            person[survey[i][1]] += Math.abs(choice);
         }
     }
-    category.forEach((e) => {
-        if(person[e.split("")[0]] > person[e.split("")[1]]) {
-            answer += e.split("")[0]
-        } else if(person[e.split("")[0]] < person[e.split("")[1]]) {
-            answer += e.split("")[1]
-        } else {
-            answer += e.split("").sort()[0]
-        }
-    })
+    for(let i=0; i<personality.length; i++) {
+        const [x,y] = personality[i].split("");
+        if(person[x] > person[y]) answer += x;
+        else if(person[x] < person[y]) answer += y;
+        else answer += x
+    }
     return answer;
 }
