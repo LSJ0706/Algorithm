42e766276caec559b8d159573e74fe72c4e456af
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-06 17:08:21 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,19 +1,10 @@
 function solution(new_id) {
-    var answer = '';
-    var lower = new_id.toLowerCase();
-    var lv2 = lower.match(/[a-z0-9-_.]/g).join('')
-    var lv3 = lv2.replace(/\.+/g, '.')
-    var lv4 = lv3.replace(/^\.|\.$/g, '')
-    var lv5 = lv4.length === 0 ? 'a' : lv4
-    var lv6 = lv5
-    if(lv6.length >= 16) {
-        lv6 = lv5.slice(0,15).replace(/\.+$/g,'')
-    }
-    if(lv6.length <= 2) {
-        var last = lv6.slice(-1)
-        for(let i=lv6.length; i<3; i++) {
-            lv6 += last
-        }
-    }
-    return lv6;
+    let answer = new_id.toLowerCase().replace(/[^a-z 0-9\._-]/g,"");
+    answer = answer.replace(/\.{2,}/g, ".");
+    answer = answer.replace(/^\.|\.$/g, "");
+    if(answer == "") answer += "a"
+    if(answer.length >= 16) answer = answer.substring(0,15).replace(/\.$/g,"");
+    if(answer.length <= 2) answer = answer.padEnd(3,answer[answer.length-1]);
+    
+    return answer;
 }
