4c96476272e6c210ffee7b7a671ff022542d730a
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-10 17:41:06 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,18 +1,40 @@
 function solution(today, terms, privacies) {
-    var answer = [];
-    var termObj = {}
-    const toDay = new Date(today)
-    terms.forEach((e) => {
-        var [contract, term] = e.split(" ")
-        termObj[contract] = Number(term)
-    })
-    privacies.forEach((e, idx) => {
-        var [date, contract] = e.split(" ")
-        var priDate = new Date(date)
-        priDate.setMonth(priDate.getMonth() + termObj[contract])
-        if(toDay >= priDate) {
-            answer.push(idx+1)
-        }
-    })
+    const answer = [];
+    today = today.split(".").map(v => parseInt(v));
+    terms = terms.map((v) => v.split(" "));
+    privacies.forEach((v,idx) => {
+        let [day, type] = v.split(" ");
+        terms.forEach((el) => {
+            if(el[0] == type) {
+                day = day.split(".").map(v => parseInt(v));
+                day[1] += parseInt(el[1]);
+                day[2]--;
+                
+                if(day[1] > 12) {
+                    if(day[1]%12 === 0) {
+                        day[0] += (day[1] / 12) - 1;
+                        day[1] = 12;
+                    }else {
+                        day[0] += parseInt(day[1] / 12);
+                        day[1] = day[1] % 12;
+                    }
+                }
+                
+                if(day[2] === 0) {
+                    day[1]--;
+                    day[2] = 28;
+                    if(day[1] === 0) {
+                        day[0]--;
+                        day[1] = 12;
+                    }
+                }
+            }
+        })
+
+        if(day[0] < today[0]) answer.push(idx+1);
+        else if(day[0] == today[0] && day[1] < today[1]) answer.push(idx+1);
+        else if(day[0] == today[0] && day[1] == today[1] && day[2] < today[2]) answer.push(idx+1);
+    });
+    
     return answer;
 }
