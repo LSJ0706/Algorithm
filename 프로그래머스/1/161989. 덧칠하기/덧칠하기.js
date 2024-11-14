40b263697a693448b835d19a39f0d5af7af076b4
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-14 14:28:56 +0900

    [level 1] Title: 덧칠하기, Time: 2.72 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,10 +1,11 @@
 function solution(n, m, section) {
     let answer = 0;
-    let painted = 0;
-    for(let v of section) {
-        if(v > painted){
+    let paint = 0;
+    for(let i=0; i<section.length; i++) {
+        if(paint < section[i]) {
+            paint = section[i];
+            paint = paint + m - 1;
             answer++
-            painted = v + m - 1;
         }
     }
     return answer;
