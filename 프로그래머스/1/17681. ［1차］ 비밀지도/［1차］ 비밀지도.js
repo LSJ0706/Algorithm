660d6f9a131aaa2f57eee9cec062f60fc0a80517
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-14 14:21:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,14 +1,13 @@
 function solution(n, arr1, arr2) {
     const answer = [];
-    arr1 = arr1.map((v) => v.toString(2).padStart(n,'0'));
-    arr2 = arr2.map((v) => v.toString(2).padStart(n,'0'));
-    for(let i=0; i<n; i++) {
-        let map = '';
+    arr1 = arr1.map((v) => v.toString(2).padStart(n,0));
+    arr2 = arr2.map((v) => v.toString(2).padStart(n,0));
+    for(let i=0; i<arr1.length; i++) {
+        let temp = "";
         for(let j=0; j<n; j++) {
-            if (arr1[i][j] == 1 || arr2[i][j] == 1) map += '#'
-            else map += ' '
+            temp += parseInt(arr1[i][j]) + parseInt(arr2[i][j]) < 1 ? " " : "#"
         }
-        answer.push(map)
+        answer.push(temp);
     }
     return answer;
 }
