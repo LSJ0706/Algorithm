d249cf111b1bd56ecff4b95635f89e16f8a82c08
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-14 15:27:28 +0900

    [level 1] Title: 대충 만든 자판, Time: 45.53 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,20 +1,16 @@
 function solution(keymap, targets) {
-    const answer = [];
-    targets.forEach((target) => {
-        let sum = 0;
-        for(let i=0; i<target.length; i++) {
-            const min = [];
+    var answer = [];
+    targets.forEach((v) => {
+        let cnt = 0;
+        for(let i=0; i<v.length; i++) {
+            const idx = [];
             for(let j=0; j<keymap.length; j++) {
-                if(keymap[j].indexOf(target[i]) != -1) min.push(keymap[j].indexOf(target[i])+1);
+                if(keymap[j].includes(v[i])) idx.push(keymap[j].indexOf(v[i])+1)
             }
-            if(min.length != 0) sum += Math.min(...min);
-            else {
-                answer.push(-1)
-                sum = 0;
-                break;
-            }
+            cnt += Math.min(...idx)
         }
-        if (sum != 0) answer.push(sum);
+        if(cnt === Infinity) answer.push(-1);
+        else answer.push(cnt);
     })
     return answer;
 }
