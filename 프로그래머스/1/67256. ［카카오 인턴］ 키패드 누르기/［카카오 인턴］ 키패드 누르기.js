137a0e8bbcd1a21e6d02cfb92011fc183e4a2ef2
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-14 16:40:41 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,41 +1,46 @@
 function solution(numbers, hand) {
-    var pad = {
-        1: [0,0], 2: [0,1], 3: [0,2],
-        4: [1,0], 5: [1,1], 6: [1,2],
-        7: [2,0], 8: [2,1], 9: [2,2],
-        '*': [3,0], 0: [3,1], '#': [3,2]
+    let answer = '';
+    let [Rhand, Lhand] = ["#","*"];
+    const pad = {
+        1:[0,0],
+        2:[0,1],
+        3:[0,2],
+        4:[1,0],
+        5:[1,1],
+        6:[1,2],
+        7:[2,0],
+        8:[2,1],
+        9:[2,2],
+        '*':[3,0],
+        0:[3,1],
+        '#':[3,2]
     }
-    var [lH, rH] = ['*','#']
-    const nearHand = (num,l,r,hand) => {
-        var lDis = Math.abs(pad[num][0] - pad[l][0]) +  Math.abs(pad[num][1] - pad[l][1])
-        var rDis = Math.abs(pad[num][0] - pad[r][0]) +  Math.abs(pad[num][1] - pad[r][1])
-        if(lDis > rDis) {
-            return 'R'
-        }else if (lDis < rDis) {
-            return 'L'
+    numbers.forEach((v) => {
+        if(v === 1 || v === 4 || v === 7) {
+            answer += "L";
+            Lhand = v;
+        }else if (v === 3 || v === 6 || v === 9) {
+            answer += "R";
+            Rhand = v;
         }else {
-            return hand === 'right' ? 'R' : 'L'
-        }
-    }
-    
-    var answer = '';
-    numbers.forEach((e) => {
-        if(e === 1 || e === 4 || e === 7) {
-            answer += 'L'
-            lH = e
-        }else if (e === 3 || e === 6 || e === 9){
-            answer += 'R'
-            rH = e
-        }else {
-            if(nearHand(e,lH,rH,hand) === 'L') {
-                answer += 'L'
-                lH = e
+            const Llen = Math.abs(pad[v][0]-pad[Lhand][0]) + Math.abs(pad[v][1]-pad[Lhand][1]);
+            const Rlen = Math.abs(pad[v][0]-pad[Rhand][0]) + Math.abs(pad[v][1]-pad[Rhand][1]);
+            if(Llen < Rlen) {
+                Lhand = v;
+                answer += "L";
+            }else if(Llen > Rlen) {
+                Rhand = v;
+                answer += "R";
             }else {
-                answer += 'R'
-                rH = e
+                if(hand === "right") {
+                    Rhand = v;
+                    answer += "R";
+                }else {
+                    Lhand = v;
+                    answer += "L";  
+                }
             }
         }
-        console.log(lH, rH)
     })
     return answer;
 }
