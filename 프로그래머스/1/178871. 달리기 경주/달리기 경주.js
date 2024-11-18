2c96907546ab8a01f4913dc4cd370879ef036f1d
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-18 18:24:21 +0900

    [level 1] Title: 달리기 경주, Time: 201.46 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,18 +1,16 @@
 function solution(players, callings) {
-    const rate = {};
-    for(let i=0; i<players.length; i++) {
-        rate[players[i]] = i;
-    };
-    
+    const playersObj = players.reduce((acc,cur,idx) => {
+        acc[cur] = idx
+        return acc;
+    },{});
     callings.forEach((v) => {
-        const tempIdx = rate[v];
-        const next = players[tempIdx-1];
+        const temp = playersObj[v];
+        const next = playersObj[v]-1;
+        players[temp] = players[next];
+        players[next] = v;
         
-        players[tempIdx-1] = v;
-        players[tempIdx] = next;
-        
-        rate[v] = tempIdx-1;
-        rate[next] = tempIdx;
+        playersObj[v]--;
+        playersObj[players[temp]] ++;
     })
     return players;
 }
