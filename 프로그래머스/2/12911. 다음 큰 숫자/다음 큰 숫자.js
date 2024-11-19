dca516c80aa2b6739e35e459e586d83a5f86a411
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-19 17:06:11 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,12 +1,7 @@
 function solution(n) {
-    var answer = 0;
-    var l = [...n.toString(2)].filter(x=> x== 1).length;
+    const len = n.toString(2).split("").filter(v => v === "1").length;
     while(true) {
         n++
-        var nl = [...n.toString(2)].filter(x=> x== 1).length;
-        if(l === nl) {
-            return n
-            break;
-        }
+       if (len === n.toString(2).split("").filter(v => v === "1").length) return n
     }
 }
