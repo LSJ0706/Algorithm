29f1121758610b743c0aa29ae7f41221e8c9724d
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-11-20 17:42:44 +0900

    [level 2] Title: 귤 고르기, Time: 92.34 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,16 +1,16 @@
 function solution(k, tangerine) {
-    var obj = {}
-    var answer = 0;
-    var sum = 0;
-    tangerine.forEach((e) => {
-        obj[e] = ++obj[e] || 1;
-    });
-    const kind = Object.values(obj).sort((a,b) => b-a);
-    for(let num of kind) {
-        answer++
-        sum += num
-        if(sum>= k) break;
+    let answer = 0;
+    let sum = 0;
+    const tangerineObj = tangerine.reduce((acc,cur) => {
+        if(acc[cur] === undefined) acc[cur] = 1;
+        else acc[cur]++;
+        return acc;
+    },{});
+    const tan = Object.values(tangerineObj).sort((a,b) => b-a);
+    for(let i=0; i<tan.length; i++) {
+        answer++;
+        sum += tan[i];
+        if(sum >= k) break;
     }
-    
     return answer;
 }
