0ad78d46c64bdbfda2ff9e5b9144a0a525e433c8
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-02 23:54:17 +0900

    [level 2] Title: 기능개발, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.13 ms
+메모리: 33.5 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 13:31:44
+2024년 12월 02일 23:54:15
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,20 +1,20 @@
 function solution(progresses, speeds) {
     let answer = [];
+    let stack = [];
     
-    while (speeds.length >0) {
-        let cnt = 0;
-        for(let i=0; i< speeds.length; i++) {
-            if(progresses[i] < 100) {
-                progresses[i] += speeds[i];
-            } 
+    while(progresses.length != 0) {
+        for(let i=0; i<progresses.length; i++) {
+            progresses[i] += speeds[i];
         }
+        
         while(progresses[0] >= 100) {
-            progresses.shift();
+            stack.push(progresses.shift());
             speeds.shift();
-            cnt++
         }
-        if(cnt > 0) {
-            answer.push(cnt)
+        
+        if(stack.length != 0) {
+            answer.push(stack.length);
+            stack = [];
         }
     }
     return answer;
