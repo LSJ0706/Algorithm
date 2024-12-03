95d7c3a374c432a8a0e017a5a4a0eb3cb29fde13
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-03 19:17:51 +0900

    [level 2] Title: 튜플, Time: 147.02 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,14 +1,9 @@
 function solution(s) {
-    var answer = [];
-    var arr = [];
-    s = s.replace('{{', "").replace('}}',"").split("},{").sort((a,b) => a.length-b.length)
-    for(let i=0; i<s.length; i++) {
-        arr.push(s[i].split(","))
-    }
-    arr.forEach((e) => {
-        for(let i=0; i<e.length; i++) {
-            if(!answer.includes(e[i])) answer.push(e[i])
-        }
+    const answer = [];
+    s = s.replace("{{", "").replace("}}","").split("},{").sort((a,b) => a.length-b.length);
+    s.forEach((v,idx) => {
+        v = v.split(",");
+        answer.push(v.filter((el) => !answer.includes(el))[0])
     })
-    return answer.map((x) => Number(x));
+    return answer.map(Number);
 }
