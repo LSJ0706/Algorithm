5b820df74a040b3518f4b1296af2e2382a1d75c2
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-03 18:57:42 +0900

    [level 2] Title: 피로도, Time: 6.51 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,17 @@
 function solution(k, dungeons) {
-    var answer = [];
-    var visited = new Array(dungeons.length).fill(false)
-    function DFS (fatigue, count) {
-        answer.push(count);
-        
-        for(let i=0; i<dungeons.length; i++) {
-            let current = dungeons[i];
-            if(fatigue >= current[0] && !visited[i]) {
+    const answer = [];
+    const visited = new Array(dungeons.length).fill(false);
+    function DFS(tired, cnt) {
+        answer.push(cnt);
+        for(let i=0; i<visited.length; i++) {
+            if(!visited[i] && tired >= dungeons[i][0]) {
                 visited[i] = true;
-                DFS(fatigue-current[1], count+1);
+                DFS(tired-dungeons[i][1], cnt+1);
                 visited[i] = false;
             }
+            
         }
     }
-    DFS(k, 0)
+    DFS(k, 0);
     return Math.max(...answer);
 }
