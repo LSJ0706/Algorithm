0f56f8be51d8dab674f98a40af00ca9e96097e93
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-04 19:06:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,17 +1,14 @@
 function solution(n,a,b) {
-    var answer = 1;
-    for(let i=0; i<Math.log2(n); i++) {
-        if(a%2 == 1) {
-            if(b == a+1) {
-                return answer;
-            }
-        }else {
-            if(a == b+1) {
-                return answer;
-            }
+    let answer = 0;
+    while(true) {
+        answer++;
+        if(Math.ceil(a/2) === Math.ceil(b/2)) break;
+        else {
+            n = n/2;
+            a = a%2 == 0 ? a/2 : (a+1)/2;
+            b = b%2 == 0 ? b/2 : (b+1)/2;
         }
-        a = Math.ceil(a/2)
-        b = Math.ceil(b/2)
-        answer++
+        
     }
+    return answer;
 }
