2ddea40bf369c98a4c557071bb1bd6efb9e3b33c
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-04 17:58:21 +0900

    [level 2] Title: 구명보트, Time: 33.39 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,13 +1,16 @@
 function solution(people, limit) {
     let answer = 0;
-    let [left, right] = [0, people.length-1];
-    people = people.sort((a,b) => b-a);
-    while(left <= right) {      
-        if(people[left] + people[right] <= limit) {
+    people = people.sort((a,b) => a-b);
+    let [left, right] = [0,people.length-1];
+    while(left <= right) {
+        if(people[left] + people[right] > limit) {
             right--;
+            answer++;
+        }else {
+            right--;
+            left++;
+            answer++;
         }
-        left++;
-        answer++;
     }
     return answer;
 }
