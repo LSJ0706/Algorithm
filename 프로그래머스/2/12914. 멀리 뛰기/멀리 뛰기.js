5a0827b9c66bf139fd58ac43eebe739a67695f6e
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-05 18:53:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,10 +1,11 @@
 function solution(n) {
-    var answer = 0;
-    var DP = new Array(n).fill(0);
-    DP[0] = 1
-    DP[1] = 1
+    const arr = new Array(n+1);
+    arr[0] = 1;
+    arr[1] = 1;
+    
     for(let i=2; i<=n; i++) {
-        DP[i] = (DP[i-1] + DP[i-2])%1234567  
+        arr[i] = arr[i-2]%1234567 + arr[i-1]%1234567;
     }
-    return DP[n];
+    
+    return arr[n]%1234567;
 }
