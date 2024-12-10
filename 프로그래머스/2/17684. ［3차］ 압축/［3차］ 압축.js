61b56d90bb65f53c2bde99882987cc9ac85c07e6
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-10 14:27:13 +0900

    [level 2] Title: [3차] 압축, Time: 2.73 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,19 +1,17 @@
 function solution(msg) {
     const words = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     const answer = [];
-    while(msg.length !== 0) {
-        let w="";
-        for(let i=1; i<=msg.length; i++) {
-            let c = msg.slice(0,i);
-            if(!words.includes(c)) {
-                words.push(c);
-                break;
-            }
-            w = c;
+    let tempword = "";
+    for(let i=0; i<msg.length; i++) {
+        tempword += msg[i];
+        if(!words.includes(tempword + msg[i+1])) words.push(tempword + msg[i+1]);
+        else {
+            continue;
         }
-        answer.push(words.indexOf(w)+1);
-        msg = msg.substr(w.length)
+        if(words.includes(tempword)) {
+            answer.push(words.indexOf(tempword)+1);
+            tempword = "";
+        }
     }
-
     return answer;
 }
