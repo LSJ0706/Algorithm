896ebbb67dd9a94a1898a075b731b2fb5719998f
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-11 20:52:42 +0900

    [level 2] Title: 방문 길이, Time: 0.66 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,15 +1,28 @@
 function solution(dirs) {
-    const direction = {U:[0,1], D:[0,-1], L:[-1,0], R:[1,0]};
-    let now = [0,0];
-    const map = new Set();
-    
-    for(let dir of dirs) {
-        let nowX = now[0] + direction[dir][0];
-        let nowY = now[1] + direction[dir][1];
-        if(nowX > 5 || nowX < -5 || nowY > 5 || nowY < -5) continue;
-        map.add("" + now[0] + now[1] + nowX + nowY)
-        map.add("" + nowX + nowY + now[0] + now[1])
-        now = [nowX, nowY];
+    let x = 0;
+    let y = 0;
+    const directions = {
+        'U' : [0,1], 
+        'D' : [0,-1], 
+        'R' : [1,0], 
+        'L' : [-1,0]
+    };
+    const visited = new Set();
+    for(let i=0; i<dirs.length; i++) {
+        if(x+directions[dirs[i]][0] <= 5 && 
+           x+directions[dirs[i]][0] >= -5 && 
+           y+directions[dirs[i]][1] <= 5 && 
+           y+directions[dirs[i]][1] >= -5) {
+            
+            let tempX = x + directions[dirs[i]][0];
+            let tempY = y + directions[dirs[i]][1];
+            
+            visited.add(`${x}, ${y}, ${tempX}, ${tempY}`);
+            visited.add(`${tempX}, ${tempY}, ${x}, ${y}`);
+            
+            x = tempX;
+            y = tempY;
+        }
     }
-    return map.size/2;
+    return visited.size/2;
 }
