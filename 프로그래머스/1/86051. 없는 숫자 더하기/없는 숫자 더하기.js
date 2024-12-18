c70db306ae61b8ad2fbd2a9fbc983bd19b96ea4a
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-18 16:49:23 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,8 +1,3 @@
 function solution(numbers) {
-    let answer = 0;
-    const arr = Array.from(new Array(10), (v,i) => v = i);
-    for(let i=0; i<arr.length; i++) {
-        if(!numbers.includes(arr[i])) answer += arr[i];
-    }
-    return answer;
+    return 45 - numbers.reduce((acc,cur) => acc += cur, 0)
 }
