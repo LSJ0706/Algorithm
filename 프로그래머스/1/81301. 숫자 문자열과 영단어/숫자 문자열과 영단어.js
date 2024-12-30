0d26a94cee5e0d9236bd3d6a9ab0b06bac64a503
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-30 09:18:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,19 +1,18 @@
 function solution(s) {
-    const answer = [];
-    const number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    let string = '';
-    [...s].forEach((v) => {
-        if(!isNaN(v)) {
-            answer.push(v);
-        } else {
-            string += v;
-            console.log(string)
-            if(number.includes(string)) {
-                answer.push(number.indexOf(string));
-                string = '';
+    let answer = "";
+    let temp = "";
+    const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    s.split("").forEach((v) => {
+        if(isNaN(v)) {
+            temp += v;
+            if(num.includes(temp)) {
+                answer += num.indexOf(temp);
+                temp = "";
             }
         }
+        else {
+            answer += v;
+        }
     })
-    
-    return parseInt(answer.join(''));
+    return Math.floor(answer);
 }
