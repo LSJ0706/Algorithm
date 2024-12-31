fabff89cb6219992be4cec664392d357bbdb70c8
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2024-12-31 16:48:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,8 @@
 function solution(absolutes, signs) {
     let answer = 0;
     absolutes.forEach((v,idx) => {
-        if(signs[idx]) {
-            answer += v
-        }else {
-            answer -= v
-        }
+        if(signs[idx]) answer += v;
+        else answer -= v;
     })
     return answer;
 }
