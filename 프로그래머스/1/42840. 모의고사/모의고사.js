e30129a7e5b0ed7bda83f7b2edb7bf44cb35cfe9
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-03 09:29:51 +0900

    [level 1] Title: 모의고사, Time: 1.83 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,19 +1,18 @@
 function solution(answers) {
-    const answer = [0, 0, 0];
-    const arr1 = [1, 2, 3, 4, 5];
-    const arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    const arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const answer = [];
+    const cnt = [0, 0, 0];
+    const one = [1, 2, 3, 4, 5];
+    const two = [2, 1, 2, 3, 2, 4, 2, 5];
+    const three = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    answers.forEach((v,i) => {
-        if (arr1[i % arr1.length] == v) answer[0]++
-        if (arr2[i % arr2.length] == v) answer[1]++
-        if (arr3[i % arr3.length] == v) answer[2]++
+    answers.forEach((v, idx) => {
+        if(v === one[idx % one.length]) cnt[0] ++;
+        if(v === two[idx % two.length]) cnt[1] ++;
+        if(v === three[idx % three.length]) cnt[2] ++;
     })
-    
-    const max = Math.max(...answer);
-    
-    return answer.reduce((acc, cur, idx) => {
-        if(cur == max) acc.push(idx+1);
-        return acc;
-    },[]);
+    const max = Math.max(...cnt);
+    for(let i=0; i<cnt.length; i++) {
+        if(cnt[i] === max) answer.push(i+1);
+    }
+    return answer;
 }
