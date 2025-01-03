61d3a7d1581789e3142188a32ff95102abdcb89f
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-03 20:24:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,11 +1,14 @@
 function solution(lottos, win_nums) {
     const answer = [];
-    const rate = [[1,6],[2,5],[3,4],[4,3],[5,2],[6,1],[6,0]];
-    const zero = lottos.filter((v) => v === 0).length;
-    const right = win_nums.filter((v) => lottos.includes(v)).length;
-    rate.forEach((v) => {
-        if(v[1] == (right+zero)) answer.push(v[0])
-        if(v[1] == right) answer.push(v[0])
-    })
-    return answer;
+    const rate = {
+        6 : 1,
+        5 : 2,
+        4 : 3,
+        3 : 4,
+        2 : 5,
+        1 : 6,
+        0 : 6
+    }
+    const winning = win_nums.filter(v => lottos.includes(v));
+    return [rate[winning.length + lottos.filter(v => v === 0).length], rate[winning.length]];
 }
