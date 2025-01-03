9d7bbd00b860b8259531cd5b7c5d49b21117bfba
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-03 09:35:46 +0900

    [level 1] Title: 소수 만들기, Time: 5.60 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,7 +1,7 @@
-function demical(num) {
-    let flag = true
-    for(let i=2; i<=Math.sqrt(num); i++) {
-        if(num%i == 0) {
+function isDecimal (num) {
+    let flag = true;
+    for(let i=2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) {
             flag = false;
             break;
         }
@@ -11,13 +11,10 @@ function demical(num) {
 
 function solution(nums) {
     let answer = 0;
-    nums = nums.sort((a,b) => a-b)
     for(let i=0; i<nums.length-2; i++) {
         for(let j=i+1; j<nums.length-1; j++) {
             for(let k=j+1; k<nums.length; k++) {
-                if(demical(nums[i] + nums[j] + nums[k])) {
-                   answer++
-                }
+                if(isDecimal(nums[i] + nums[j] + nums[k])) answer++;
             }
         }
     }
