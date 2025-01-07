03405687da62efe33edcc741e0d697241671e15b
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-07 10:09:33 +0900

    [level 1] Title: 체육복, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,14 +1,18 @@
 function solution(n, lost, reserve) {
     let answer = 0;
-    const arrLost = lost.filter((v) => !reserve.includes(v)).sort((a,b) => a-b);
-    const arrReserve = reserve.filter((v) => !lost.includes(v)).sort((a,b) => a-b);
-    arrLost.forEach((v) => {
-        if (arrReserve.includes(v+1) || arrReserve.includes(v-1)) {
-            answer++
-            arrReserve.shift();
+    const newReserve = reserve.sort((a,b) => a-b).filter(v => !lost.includes(v));
+    let newLost = lost.sort((a,b) => a-b).filter(v => !reserve.includes(v));
+    answer = n - newLost.length;
+    for(let i=0; i<newReserve.length; i++) {
+        if(newLost.includes(newReserve[i]-1)) {
+            newLost = newLost.filter(v => v !== newReserve[i]-1);
+            answer++;
+        }else if(newLost.includes(newReserve[i]+1)) {
+            newLost = newLost.filter(v => v !== newReserve[i]+1);
+            answer++;
+        }else if(newReserve.length === 0) {
+            break;
         }
-    })
-    
-    
-    return answer + n - arrLost.length;
+    }
+    return answer;
 }
