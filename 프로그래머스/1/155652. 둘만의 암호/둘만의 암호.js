ae53d5e01d6e543c3c0065f45fcf9b688554436f
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-10 09:22:23 +0900

    [level 1] Title: 둘만의 암호, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,16 +1,10 @@
 function solution(s, skip, index) {
-    const answer = [];
-    const alpha =  ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-                    'n','o','p','q','r','s','t','u','v','w','x','y','z',];
-    const filter = alpha.filter((v) => !skip.includes(v));
-    for(let i=0; i<s.length; i++) {
-        let temp = filter.indexOf(s[i]);
-        
-        if(temp + index >= filter.length) temp = (temp+index)%filter.length;
-        else temp = temp + index;
-        
-        answer.push(filter[temp]);
-    }
-    return answer.join("");
+    let answer = '';
+    const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const arr = alpha.filter(v => !skip.includes(v));
+    s.split("").forEach((v) => {
+        const num = arr.indexOf(v) + index  >= arr.length ? (arr.indexOf(v) + index) % arr.length : arr.indexOf(v) + index
+        answer += arr[num]
+    })
+    return answer;
 }
-//alpha 26
