f880534dbb49482ffaf7b281b68cc50ad690a557
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-13 09:35:31 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,18 +1,13 @@
 function solution(wallpaper) {
-    const code = [];
-    let lux, luy, rdx, rdy;
-    const coordinate = wallpaper.map((v) => v.split(""));
-    for(let i=0; i<coordinate.length; i++) {
-        for(let j=0; j<coordinate[0].length; j++) {
-            if(coordinate[i][j] == '#') code.push([i,j]);
-        }
-    }
-    code.forEach((v) => {
-        lux = lux <= v[0] ? lux : v[0];
-        luy = luy <= v[1] ? luy : v[1];
-        rdx = rdx >= v[0] ? rdx : v[0];
-        rdy = rdy >= v[1] ? rdy : v[1];
-    })
-    
-    return [lux, luy, rdx+1, rdy+1];
+    const x = [];
+    const y = [];
+    wallpaper.forEach((v,row) => {
+        v.split("").forEach((el,col) => {
+            if(el === "#") {
+                x.push(row);
+                y.push(col);
+            }
+        });
+    });
+    return [Math.min(...x),Math.min(...y),Math.max(...x)+1,Math.max(...y)+1];
 }
