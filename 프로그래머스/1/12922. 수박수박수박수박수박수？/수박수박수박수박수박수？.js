03adc3eb8af68ad6c9ac5c2cce8492a3798ef3e1
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-16 19:32:48 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    let answer = "수";
-    for(let i=2; i<=n; i++) {
-        if(i%2 === 0) answer += "박"
-        else answer += "수"
-    }
-    return answer;
+    return "수박".repeat(n).slice(0,n);
 }
