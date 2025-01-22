877287309cf90077df93e52cd2f59a84f0206ba4
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-22 21:08:50 +0900

    [level 1] Title: 신고 결과 받기, Time: 195.77 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,30 +1,26 @@
 function solution(id_list, report, k) {
-    
-    const reportList = id_list.reduce((acc,cur) => {
-        acc.set(cur,0);
-        return acc;
+    const idMap = id_list.reduce((acc,cur) => {
+        acc.set(cur, []);
+        return acc
     },new Map());
     
-    const idList = id_list.reduce((acc,cur) => {
-        acc.set(cur,[]);
-        return acc;
-    },new Map());
+    const setReport = new Set(report);
     
-    report.forEach((v) => {
-        const [report, arrest] = v.split(" ");
-        if(!idList.get(report).includes(arrest)) {
-            idList.get(report).push(arrest);
-            reportList.set(arrest, reportList.get(arrest)+1);
+    const reportMap = [...setReport].reduce((acc,cur) => {
+        const [reporting, reported] = cur.split(" ");
+        idMap.get(reporting).push(reported);
+        acc.set(reported, (acc.get(reported) || 0) +1);
+        return acc;
+    }, new Map());
+    
+    const answer = id_list.map((v) => {
+        let cnt = 0;
+        for(let i=0; i<idMap.get(v).length; i++) {
+            if(reportMap.get(idMap.get(v)[i]) >= k) {
+                cnt++;
+            }
         }
+        return cnt
     })
-    
-    const answer = id_list.reduce((acc,cur) => {
-        let count = 0;
-        idList.get(cur).forEach((v) => {
-            if(reportList.get(v) >= k) count++
-        })
-        acc.push(count);
-        return acc;
-    },[])
     return answer;
 }
