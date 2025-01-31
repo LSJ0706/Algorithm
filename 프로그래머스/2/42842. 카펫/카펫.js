682f1afacb6c4d8ef2b4f4bc2ae7be4257ea4fbb
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-01-31 09:59:13 +0900

    [level 2] Title: 카펫, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,25 +1,11 @@
-function findDivisors (num) {
-    const divisors = [];
-    for(let i=2; i<=Math.sqrt(num); i++) {
-        if(num%i === 0) {
-            divisors.push(i);
-            if(num/i !== i) divisors.push(num/i);
-        } 
-    }
-    return divisors.sort((a,b) => b-a);
-}
-function solution(brown, yellow) {
-    const WholeTile = brown + yellow
-    const arr = findDivisors(WholeTile);
-    const answer = [];
-    for(let i=0; i<arr.length; i++) {
-        const realYellow = (WholeTile/arr[i]-2) * (arr[i]-2);
-        const realBrown = (WholeTile/arr[i] + arr[i]) * 2 - 4;
-        if(yellow === realYellow && brown === realBrown) {
-            answer.push(arr[i]);
-            answer.push(WholeTile/arr[i]);
+function solution(brown, red) {
+    var answer = [];
+    for (var i = 3; i <= (brown+red)/i; i++) {
+        var x = Math.floor((brown+red)/i);
+        if( (x-2)*(i-2)=== red) {
             break;
         }
     }
-    return answer;
+
+    return [x,i];
 }
