ce9e261ba718b130df81548408f2bb6cdbe061ef
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-02-01 12:36:45 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.13 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 04일 19:06:26
+2025년 02월 01일 12:36:42
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,14 +1,9 @@
-function solution(n,a,b) {
+function solution(n,a,b){
     let answer = 0;
-    while(true) {
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
         answer++;
-        if(Math.ceil(a/2) === Math.ceil(b/2)) break;
-        else {
-            n = n/2;
-            a = a%2 == 0 ? a/2 : (a+1)/2;
-            b = b%2 == 0 ? b/2 : (b+1)/2;
-        }
-        
     }
     return answer;
 }
