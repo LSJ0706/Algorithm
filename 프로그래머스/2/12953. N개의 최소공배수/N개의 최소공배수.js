3a007859380d7915e9b4c94681536ae9f521669d
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-02-03 14:24:20 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,17 +1,6 @@
 function solution(arr) {
-    let answer = 0;
-    let num = arr[0]
-    for(let i=1; i<arr.length; i++) {
-        let GCD = calGCD(num,arr[i]);
-        let LCM = (num * arr[i]) / GCD;
-        num = LCM;
-    }
-    return num;
+    return arr.reduce((acc, cur) => (acc*cur) / calGCD(acc,cur))
 }
-function calGCD(x,y) {
-    if(x%y != 0) {
-        return calGCD(y, x%y)
-    }else if(x%y === 0){
-        return y
-    }
+function calGCD (a,b) {
+    return a%b ? calGCD(b,a%b) : b
 }
