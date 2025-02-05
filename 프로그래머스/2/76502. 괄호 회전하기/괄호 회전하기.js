8e1f9ff10b3012f7d0664e978efb1d3e19fdf21b
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-02-05 09:51:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 382.88 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,20 +1,21 @@
 function solution(s) {
-    var answer = 0;
-    for(let i=0; i<s.length; i++) {
-        var stack = [];
-        [...s].forEach((e) => {
-            stack.push(e)
-            if(stack.slice(-2).join("") == "[]" || 
-               stack.slice(-2).join("") == "()" || 
-               stack.slice(-2).join("") == "{}" ) {
-                stack.pop()
-                stack.pop()
+    let answer = 0;
+    let str = s;
+
+    for(let i=0; i<str.length; i++) {
+        const stack = [];
+        str = str.slice(1,str.length) + str[0];
+        str.split("").forEach((v) => {
+            stack.push(v);
+            if(stack.slice(-2).join("") === "[]" ||
+               stack.slice(-2).join("") === "{}" ||
+               stack.slice(-2).join("") === "()"
+              ) {
+                stack.pop();
+                stack.pop();
             }
         })
-        if(stack.length === 0) {
-            answer++
-        }
-        s = s.slice(1,s.length) + s.slice(0,1)
+        if(stack.length === 0) answer++;
     }
     return answer;
 }
