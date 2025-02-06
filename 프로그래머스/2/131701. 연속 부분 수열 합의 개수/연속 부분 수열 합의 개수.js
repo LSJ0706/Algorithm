9cd0dbf7b89b871a960b004b54e3e83531529024
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-02-06 10:21:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 871.55 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,20 +1,11 @@
 function solution(elements) {
-    let answer = 0;
-    let len = elements.length;
-    const set = new Set();
-    for(let i=1; i<=len; i++) {
-        let sum = 0;
-        for(let j=0; j<len; j++) {
-            if(j === 0) {
-                for(let k=0; k<i; k++) {
-                    sum += elements[k];
-                }
-            }else {
-                sum -= elements[j];
-                sum += elements[(j+i-1)%len];
-            }
-            set.add(sum);
+    const circularQueue = [...elements,...elements];
+    const answer = [];
+    for(let i=1; i<=elements.length; i++) {
+        for(let j=0; j<elements.length; j++) {
+            answer.push(circularQueue.slice(j,j+i).reduce((acc,cur) => acc + cur))
         }
     }
-    return set.size;
+    const answerSet = new Set(answer);
+    return [...answerSet].length;
 }
