56995d7f07954418576426dac126feee7927d2d2
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-02-07 09:29:37 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,11 +1,9 @@
 function solution(citations) {
-    var answer = 0;
-    citations = citations.sort((a,b) => b-a)
+    let answer = 0;
+    citations.sort((a,b) => b-a);
     for(let i=0; i<citations.length; i++) {
-        if(citations[i] <= i) {
-            break;
-        }
-        answer++
+        if(citations[i] <= answer) break;
+        answer++;
     }
     return answer;
 }
