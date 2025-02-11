b4f81d7b4c0693166c3ffde4711204f05d0701ed
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-02-11 09:54:15 +0900

    Update 행렬의 곱셈.js

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,5 +1,5 @@
 function solution(arr1, arr2) {
-    const answer = [];
+    let answer = [];
     for(let i=0; i<arr1.length; i++) {
         const arr = [];
         for(let j=0; j<arr2[0].length; j++) {
