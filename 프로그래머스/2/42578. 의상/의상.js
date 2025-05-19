51c03b2f0415c9e086320112556a3e76efed27f0
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-05-19 15:35:23 +0900

    [level 2] Title: 의상, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,10 +1,13 @@
 function solution(clothes) {
-    let answer = 0;
-    const obj = {};
-    clothes.map(([clothe, body]) => {
-        if(obj[body] === undefined) obj[body] = 1
-        else obj[body]++
-    });
-    answer = Object.values(obj).reduce((acc,cur) => acc * (cur+1),1)
+    let answer = 1;
+    const arr = clothes.reduce((acc,cur) => {
+        const [_, type] = cur
+        if(acc[type] === undefined) acc[type] = 1
+        else acc[type] = acc[type]+1;
+        return acc
+    },{})
+    for(let type in arr) {
+        answer *= (arr[type]+1)
+    }
     return answer-1;
 }
