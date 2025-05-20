6e446d253a9fd5700b7cdded1c307fbd5998a52b
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-05-20 19:13:15 +0900

    [level 2] Title: 전화번호 목록, Time: 156.49 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,15 @@
 function solution(phone_book) {
-    phone_book.sort();
-    let answer = phone_book.some((v, idx) => {
-        return idx+1 < phone_book.length && phone_book[idx+1].startsWith(v);
-    });
-    return !answer;
+    let answer = true;
+    const phone_nums = phone_book.sort();
+    for(let i=0; i<phone_nums.length; i++) {
+        if(i === phone_nums.length-1) {
+            answer = true;
+            break;
+        }
+        if(phone_nums[i+1].startsWith(phone_nums[i])) {
+            answer = false;
+            break;
+        }
+    }
+    return answer;
 }
