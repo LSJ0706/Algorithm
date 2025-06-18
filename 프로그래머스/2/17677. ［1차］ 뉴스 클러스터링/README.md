63edeab149e3c0970489c3e748c27ec279c20a86
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-06-18 20:39:38 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.69 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 26.72 ms
+메모리: 33.6 MB, 시간: 0.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 13:02:50
+2025년 06월 18일 20:39:35
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,32 +1,25 @@
+const sliceStr = (str,reg) => {
+    const arr = [];
+    for(let i=0; i<str.length-1; i++) {
+        const word = str.slice(i,i+2);
+        if(reg.test(word)) arr.push(word);
+    }
+    return arr
+}
+
 function solution(str1, str2) {
-    const str1Arr = [];
-    const str2Arr = [];
-    const regal = /^[a-z]+$/;
+    let sub = 0;
+    const reg = new RegExp(/^[a-z]*$/);
     
-    str1 = str1.toLowerCase();
-    str2 = str2.toLowerCase();
+    const strArr1 = sliceStr(str1.toLowerCase(),reg);
+    const strArr2 = sliceStr(str2.toLowerCase(),reg);
     
-    for(let i=0; i<str1.length-1; i++) {
-        if(regal.test(str1.slice(i,i+2))) {
-            str1Arr.push(str1.slice(i,i+2));
-        }
+    for(let i=0; i<strArr1.length; i++) {
+        if(strArr2.includes(strArr1[i])) {
+            strArr2.splice(strArr2.indexOf(strArr1[i]),1);
+            sub++;
+        };
     }
-    
-    for(let i=0; i<str2.length-1; i++) {
-        if(regal.test(str2.slice(i,i+2))) {
-            str2Arr.push(str2.slice(i,i+2));
-        }
-    }
-    let union = 0;
-    let intersection = 0; 
-    const set = new Set([...str1Arr, ...str2Arr]);
-    
-    set.forEach((v) => {
-        const atomic1 = str1Arr.filter((el) => el === v).length;
-        const atomic2 = str2Arr.filter((el) => el === v).length;
-        union += Math.max(atomic1, atomic2);
-        intersection += Math.min(atomic1, atomic2);
-    });
-    
-    return union === 0 ? 65536 : parseInt((intersection/union)*65536);
+    if(strArr1.length === 0 && strArr2.length === 0) return 65536
+    else return parseInt(65536 * (sub / (strArr1.length + strArr2.length)));
 }
