3e25ac57694660bf35613170085affd3f7bf3aad
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-07-14 17:44:08 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.67 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,27 +1,24 @@
 function solution(fees, records) {
-    const parking = {};
+    const answer = [];
+    const carObj = {};
     records.forEach((record) => {
-        const [time, carNum, enter] = record.split(" ");
-        const [hh, mm] = time.split(":").map(Number);
-        const minutes = hh*60 + mm;
+        const [time, carNum, exit] = record.split(" ");
+        const [mm,ss] = time.split(":").map(Number);
+        const minutes = mm*60 + ss;
         
-        if(!parking[carNum]) {
-            parking[carNum] = {time:0, carNum};
+        if(carObj[carNum] === undefined) carObj[carNum] = [undefined, 0];
+        if(exit === "IN") {
+            carObj[carNum][0] = exit;
+            carObj[carNum][1] -= minutes;
+        }else if(exit === "OUT") {
+            carObj[carNum][0] = exit;
+            carObj[carNum][1] += minutes;
         }
-        
-        parking[carNum].enter = enter;
-        
-        if(enter === "IN") {
-            parking[carNum].inTime = minutes;
-            return;
-        }
-        parking[carNum].time += minutes - parking[carNum].inTime;
     });
-    
-    const answer = Object.values(parking).sort((a,b) => a.carNum - b.carNum).map((v) => {
-        if(v.enter === "IN") v.time += 1439 - v.inTime;
-        if(fees[0] > v.time) return fees[1];
-        return fees[1] + Math.ceil((v.time - fees[0])/fees[2]) * fees[3];
-    })
+    const calFees = Object.entries(carObj).sort((a,b) =>a[0] - b[0]).forEach((v) => {
+        if(v[1][0] === "IN") v[1][1] += 1439
+        if(fees[0] >= v[1][1]) answer.push(fees[1])
+        else answer.push(fees[1] + Math.ceil((v[1][1]-fees[0])/fees[2])*fees[3])
+    });
     return answer;
 }
