b4ed7a052c45ef8d75bc7f93b9384b5c2b6e23a6
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-07 16:19:12 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 32.35 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,45 +1,48 @@
 function solution(m, n, board) {
-  let answer = 0;
-  board = board.map((v) => v.split(""));
-  while (true) {
-    const clear = [];
-    for (let i = 0; i<m-1; i++) {
-      for (let j = 0; j<n-1; j++) {
-        if (
-          board[i][j] === board[i + 1][j] &&
-          board[i][j] === board[i][j + 1] &&
-          board[i][j] === board[i + 1][j + 1] &&
-          board[i][j] !== 0
-        )
-          clear.push([i, j]);
-      }
-    }
-
-    if (clear.length === 0) break;
-
-    for (let i = 0; i < clear.length; i++) {
-      const [x, y] = clear[i];
-
-      board[x][y] = 0;
-      board[x + 1][y] = 0;
-      board[x][y + 1] = 0;
-      board[x + 1][y + 1] = 0;
-    }
-
-    for (let i = m - 1; i >= 0; i--) {
-      for (let j = 0; j < n; j++) {
-        if (board[i][j] === 0) {
-          for (let k = i - 1; k >= 0; k--) {
-            if (board[k][j] != 0) {
-                board[i][j] = board[k][j];
-                board[k][j] = 0;
-                break;
-            }
-          }
+    let answer = 0;
+    let tiles = [];
+    board = board.reduce((acc,cur) => {
+        acc.push(cur.split(""));
+        return acc;
+    },[]);
+    
+    while(true) {
+        for(let i=0; i<board.length-1; i++) {
+        for(let j=0; j<board[0].length-1; j++) {
+            if(board[i][j] === board[i][j+1] &&
+               board[i][j] === board[i+1][j] &&
+               board[i][j] === board[i+1][j+1] &&
+               board[i][j] !== '*'
+              ) tiles.push([i,j]);
         }
-      }
     }
-  }
-  answer = board.flat().filter((v) => v === 0).length;
-  return answer;
+    
+    if(tiles.length === 0) return board.flat().filter(v => v === '*').length;
+    
+    tiles.forEach((tile) => {
+        const [x, y] = tile;
+        board[x][y] = '*';
+        board[x+1][y] = '*';
+        board[x][y+1] = '*';
+        board[x+1][y+1] = '*';
+    });
+        tiles = [];
+        
+    for(let i=board.length-1; i>=0; i--) {
+        for(let j=board.length-1; j>=0; j--) {
+            if(board[i][j] === '*') {
+                for(let k=i; k>=0; k--) {
+                    if(board[k][j] !== '*') {
+                        board[i][j] = board[k][j];
+                        board[k][j] = '*';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    }
+    
+    
+    return answer;
 }
