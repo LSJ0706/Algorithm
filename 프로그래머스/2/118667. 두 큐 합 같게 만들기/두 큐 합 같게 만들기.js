516ead7300847daa825470452cab0767460458a4
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-08 17:40:01 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 26.99 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,35 +1,26 @@
 function solution(queue1, queue2) {
     let answer = 0;
-    
-    let sum1 = Math.floor(queue1.reduce((acc,cur) => acc+cur));
-    let sum2 = Math.floor(queue2.reduce((acc,cur) => acc+cur));
-    const all = sum1 + sum2;
-    
-    const queue = [...queue1, ...queue2];
-    
-    const len = queue1.length;
-    const maxLen = queue.length;
-    
-    let left = 0;
-    let right = len;
-    
-    if (all % 2 !== 0) return -1;
-    
+    const len = queue1.length + queue2.length;
+    let sum1 = queue1.reduce((acc, cur) => acc + cur, 0);
+    let sum2 = queue2.reduce((acc, cur) => acc + cur, 0);
+    let [idx1, idx2] = [0, 0];
     while(sum1 !== sum2) {
-        if(sum1 > all/2) {
-            const qNum = queue[left];
-            sum1 -= qNum;
-            sum2 += qNum;
-            left++;
-        }else {
-            const qNum = queue[right];
-            sum2 -= qNum;
-            sum1 += qNum;
-            right++;
+        if(sum1 > sum2) {
+            sum1 -= queue1[idx1];
+            sum2 += queue1[idx1];
+            queue2.push(queue1[idx1]);
+            idx1++;
+        }else if(sum1 < sum2){
+            sum2 -= queue2[idx2];
+            sum1 += queue2[idx2];
+            queue1.push(queue2[idx2]);
+            idx2++;
+        }
+        if(answer > 3 * len) {
+            return -1;
         }
         answer++;
-        if(answer >= maxLen * 3) return -1;
     }
-
+    
     return answer;
 }
