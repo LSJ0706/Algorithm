ee51727b2032a3fa6e6381b1b601255f50040812
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-20 18:06:08 +0900

    [level 2] Title: 가장 큰 수, Time: 169.98 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
     let answer = '';
-    answer = numbers.map(number=> number + '').sort((a,b) => (b+a) - (a+b)).join('');
+    answer = numbers.map((v) => v + '').sort((a,b) => (b+a) - (a+b)).join('');
     return answer[0] === '0' ? '0' : answer
 }
