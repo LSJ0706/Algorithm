5d7cad2f0824c8f9ab1353649bc771da2c290333
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-25 23:13:33 +0900

    [level 2] Title: 큰 수 만들기, Time: 76.45 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,11 @@
 function solution(number, k) {
     const answer = [];
-    
     for(let i=0; i<number.length; i++) {
-        const num = number[i];
-        while(k > 0 && answer.at(-1) < num) {
+        while(k > 0 && answer.at(-1) < number[i]) {
             answer.pop();
             k--;
         }
-        answer.push(num);
+        answer.push(number[i]);
     }
-    answer.splice(number.length-k,k);
-    return answer.join("");
+    return answer.splice(0,answer.length-k,k).join("");
 }
