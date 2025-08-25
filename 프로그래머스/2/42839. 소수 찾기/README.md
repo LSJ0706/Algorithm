518d1260ce20c199102e0f3c6a78a31b36986dd9
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-25 22:08:28 +0900

    [level 2] Title: 소수 찾기, Time: 4.73 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 5.06 ms
+메모리: 37 MB, 시간: 4.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 21일 18:19:57
+2025년 08월 25일 22:08:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,30 +1,28 @@
-function isPrime (num) {
-    if(num <= 1) return false;
-    for(let i=2; i<=Math.sqrt(num); i++) {
-        if(num % i === 0) {
-            return false;
-        }
+const isPrime = (number) => {
+    if(number <= 1) return false;
+    for(let i=2; i<=Math.sqrt(number); i++) {
+        if(number % i === 0) return false;
     }
     return true;
-} 
+}
 
 function solution(numbers) {
     let answer = 0;
-    const arr = numbers.split("");
-    const visited = new Array(arr.length).fill(false);
+    const numArr = numbers.split("");
     const set = new Set();
+    const visited = new Array(numbers.length).fill(false);
     
-    function DFS(depth, str) {
-        if(depth === arr.length) return;
-        for(let i=0; i<arr.length; i++) {
+    function DFS(num, len) {
+        if(len === numArr.length) return;
+        for(let i=0; i<numArr.length; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                set.add(parseInt(str + arr[i]));
-                DFS(depth+1, str+arr[i]);
+                set.add(parseInt(num + numArr[i]));
+                DFS(num + numArr[i], len+1);
                 visited[i] = false;
             }
         }
     }
-    DFS(0, "");
-    return [...set].filter(v => isPrime(v)).length;
+    DFS('',0);
+    return [...set].filter((v) => isPrime(v)).length;
 }
