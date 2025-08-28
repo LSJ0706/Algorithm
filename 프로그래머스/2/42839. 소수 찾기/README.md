f9c92640afde3e68e3ac3fc6552e9bb413789ff1
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-28 18:03:45 +0900

    [level 2] Title: 소수 찾기, Time: 16.55 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 4.73 ms
+메모리: 36.3 MB, 시간: 16.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 25일 22:08:25
+2025년 08월 28일 18:03:42
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,28 +1,29 @@
-const isPrime = (number) => {
-    if(number <= 1) return false;
-    for(let i=2; i<=Math.sqrt(number); i++) {
-        if(number % i === 0) return false;
+function isPrime(num) {
+    if(num <= 1) return false;
+    for(let i=2; i<=Math.sqrt(num); i++) {
+        if(num % i === 0) return false;
     }
     return true;
 }
 
 function solution(numbers) {
-    let answer = 0;
-    const numArr = numbers.split("");
-    const set = new Set();
+    const answer = new Set();
     const visited = new Array(numbers.length).fill(false);
+    const numArr = numbers.split("");
     
-    function DFS(num, len) {
-        if(len === numArr.length) return;
-        for(let i=0; i<numArr.length; i++) {
+    function DFS(str, len) {
+        if(len > visited.length) return;
+        for(let i=0; i<visited.length; i++) {
             if(!visited[i]) {
+                const newNum = str + numArr[i];
+                answer.add(parseInt(newNum));
                 visited[i] = true;
-                set.add(parseInt(num + numArr[i]));
-                DFS(num + numArr[i], len+1);
+                DFS(newNum, len+1);
                 visited[i] = false;
             }
         }
     }
+    
     DFS('',0);
-    return [...set].filter((v) => isPrime(v)).length;
+    return [...answer].filter((v) => isPrime(v)).length;
 }
