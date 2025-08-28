2a6d833dedb74378dc6c2abe141dfd9179539a82
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2025-08-28 13:09:30 +0900

    [level 2] Title: 삼각 달팽이, Time: 89.38 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,26 +1,27 @@
 function solution(n) {
-    const answer = Array.from({length: n}, () => new Array(n).fill(0));
-    let [x,y] = [-1,0];
+    const answer = Array.from({length:n}, (_,idx) => new Array(idx+1).fill(0));
+    let [x, y] = [-1 ,0];
     let cnt = 1;
     let idx = 1;
-    while(true) {
-        if(n === 0) break;
+    while(n > 0) {
         for(let i=0; i<n; i++) {
             if(idx % 3 === 1) {
                 answer[x+1][y] = cnt;
                 x++;
-            }else if(idx % 3 === 2) {
+            }
+            else if(idx % 3 === 2) {
                 answer[x][y+1] = cnt;
                 y++;
-            }else if(idx % 3 === 0) {
+            }
+            else if(idx % 3 === 0) {
                 answer[x-1][y-1] = cnt;
                 x--;
                 y--;
             }
             cnt++;
         }
-        n--;
         idx++;
+        n--;
     }
-    return answer.flat().filter(v => v != 0);
+    return answer.flat();
 }
