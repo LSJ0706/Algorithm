a80d9fed49392e4edff4b6bab5577ee2ef63415f
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2026-01-10 21:14:48 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,4 @@
 function solution(s) {
-    const arr = s.split(" ").map(Number).sort((a,b) => a-b);
-    return `${arr[0]} ${arr[arr.length-1]}`;
+    const arr = s.split(' ');
+    return `${Math.min(...arr)} ${Math.max(...arr)}`;
 }
