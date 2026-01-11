ec62320c7518c20072f0ee72354ec8a307b7ad42
Lee Seung Jun <66673374+LSJ0706@users.noreply.github.com>
2026-01-11 12:22:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 30.77 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,15 +1,15 @@
 function solution(arr1, arr2) {
-    let answer = [];
+    const answer = [];
     for(let i=0; i<arr1.length; i++) {
-        const arr = [];
+        const arr3 = [];
         for(let j=0; j<arr2[0].length; j++) {
-            let sum = 0;
+            let sum = 0
             for(let k=0; k<arr2.length; k++) {
-                sum += arr1[i][k] * arr2[k][j];
+               sum += arr1[i][k] * arr2[k][j];
             }
-            arr.push(sum);
+            arr3.push(sum);
         }
-        answer.push(arr);
+        answer.push(arr3);
     }
     return answer;
 }
